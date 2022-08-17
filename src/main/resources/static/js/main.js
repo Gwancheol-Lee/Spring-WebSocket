49538b5e6cb84a503393b61e8ad342bbacfab0d7
Gwancheol-Lee <70332526+Gwancheol-Lee@users.noreply.github.com>
2022-08-17 17:05:19 +0900

    프론트단 js 21~65 라인 중요(소켓 연결 부분)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,7 +12,7 @@ var stompClient = null;
 var stompClient2 = null;
 var username = null;
 var userId = null;
-
+ 
 var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
